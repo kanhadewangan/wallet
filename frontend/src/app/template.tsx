50a4c266224a5fc2425c1d5891dae0bb7ef0c87c
Kanha Dewangan <166921193+kanhadewangan@users.noreply.github.com>
2025-06-12 18:11:04 +0530

    Delete frontend directory

--- frontend/src/app/template.tsx
+++
@@ -1,8 +0,0 @@
-import { metadata } from './metadata'
-import RootLayout from './layout'
-
-export { metadata }
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  return <RootLayout>{children}</RootLayout>
-} 
